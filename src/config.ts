401a1117872dd074d34d0b1ac7247f1da65d3bb3
Kerem Sevencan <kerem@ritmix.org>
2021-04-14 16:47:43 +0200

    separate tempaltes

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,4 @@ export const workDir = process.cwd()
 export const adrDir = workDir + '/docs/adr/'
 
 const cliDir = path.dirname(fs.realpathSync(__filename))
-export const cliTemplatesPath = path.join(cliDir + '/templates/')
+export const cliTemplatesPath = path.join(cliDir + '/../templates/')
